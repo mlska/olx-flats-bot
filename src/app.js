eec55f7608a84859d9b4e1be47b93d1951bfff4c
Kamil <kamil.malesa1@gmail.com>
2024-10-16 19:35:04 +0200

    feat: reschedule and default config jobs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import 'dotenv/config';
-import cron from 'node-cron';
+
 import OlxBot from './classes/OlxBot.js';
+import Cron from './classes/Cron.js';
 
 const { USER_ID: messageReceiver, TELEGRAM_TOKEN: telegramToken } = process.env;
+
+const defaultSchedule = '0 7,12,17,22 * * *';
 
 const olxBot = new OlxBot({
   flatsStorageFilePath: './data/flats_ids.json',
@@ -11,7 +14,9 @@ const olxBot = new OlxBot({
   messageReceiver
 });
 
-cron.schedule('0 7,12,17,22 * * *', () => {
+const cron = new Cron();
+
+cron.startJob(defaultSchedule, () => {
   olxBot.scrap();
 });
 
@@ -22,6 +27,24 @@ olxBot.telegram.startCommandListener('@scrap', () => {
 
 olxBot.telegram.startCommandListener('@status', () => {
   olxBot.telegram.sendMessage(messageReceiver, `Online`);
+});
+
+olxBot.telegram.startCommandListener(/\@schedule (.+)/, (ctx) => {
+  const schedule = ctx.match[1];
+  const isValid = cron.validate(schedule);
+  const message = isValid ? `New scraping schedule ${schedule}` : `Incorret schedule`;
+  isValid &&
+    cron.startJob(schedule, () => {
+      olxBot.scrap();
+    });
+  olxBot.telegram.sendMessage(messageReceiver, message);
+});
+
+olxBot.telegram.startCommandListener('@default', () => {
+  cron.startJob(defaultSchedule, () => {
+    olxBot.scrap();
+  });
+  olxBot.telegram.sendMessage(messageReceiver, `Default schedule set`);
 });
 
 olxBot.telegram.start();
